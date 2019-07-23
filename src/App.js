4ad655b7ff9fb0fe11936e8246224710b50d89bf
nabendu82 <nabendu.biswas@gmail.com>
2019-07-23 20:48:50 +0530

    Login, logout and Submit enhancements

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { withAuth } from "@8base/react-sdk";
 import Routes from './router';
 import './App.css';
 import Header from './components/header';
 
-const App = () => (
+const App = ({ auth }) => console.log(auth.isAuthorized) || (
   <div className="App">
     <Router>
-      <Header />
+      <Header isAuthorized={auth.isAuthorized} />
       <Routes />
     </Router>
   </div>
 );
 
-export default App;
+export default withAuth(App);
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './index.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header id="news-header">
       <section>
@@ -15,14 +15,16 @@ const Header = () => {
               <NavLink to="/">News  |  </NavLink>
             </li>
             <li>
-              <NavLink to="/submit">Submit</NavLink>
+              {props.isAuthorized && <NavLink to="/submit">Submit</NavLink>}
+              {!props.isAuthorized && <NavLink to="/login">Submit</NavLink>}
             </li>
           </ul>
         </div>
       </section>
       <div className="auth">
         <div>
-          <NavLink to="/login">Login/Logout</NavLink>
+          {props.isAuthorized && <NavLink to="/login">Logout</NavLink>}
+          {!props.isAuthorized && <NavLink to="/login">Login</NavLink>}
         </div>
       </div>
     </header>
